8f599de5167133c793587ec3c01c3fbd7b19c5e9
afbly <afableafrahly@gmail.com>
2022-03-14 22:04:32 +0800

    fix route

--- src/sections/navbar/index.jsx
+++ src/sections/navbar/index.jsx
@@ -1,8 +1,4 @@
 import React, { useState } from 'react'
-import {
-  BrowserRouter,
-  Link
-} from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 import logo from '../../assets/media/svg/afrahly.svg'
 import cx from '../../assets/styles/sections/Navbar.module.scss'
@@ -29,40 +25,38 @@ const Navbar = () => {
   // window.addEventListener('scroll', changeBackground)
 
   return (
-    <BrowserRouter>
-      <div className={cx.navbar}>
-        <Link to='/' className={cx.navbar__logo}>
-          <img src={logo} alt="Logo"/>
-        </Link>
-        <div className={cx.navbar__hamburger} onClick={handleClick}>
-          <div className={isClicked ? cx.navbar__hamburger__line__active : cx.navbar__hamburger__line}></div>
-        </div>
-        <nav>
-          <ul className={isClicked ? cx.navbar__menu__active : cx.navbar__menu}>
-            <li className={cx.navbar__menu__link}>
-              <HashLink smooth to="/#about">
-                About
-              </HashLink>
-            </li>
-            <li className={cx.navbar__menu__link}>
-              <HashLink smooth to="/#experience">
-                Experience
-              </HashLink>
-            </li>
-            <li className={cx.navbar__menu__link}>
-              <HashLink smooth to="/#projects">
-                Projects
-              </HashLink>
-            </li>
-            <li className={cx.navbar__menu__link}>
-              <HashLink smooth to="/#contact">
-                Contact
-              </HashLink>
-            </li>
-          </ul>
-        </nav>
+    <div className={cx.navbar}>
+      <HashLink to='/#home' className={cx.navbar__logo}>
+        <img src={logo} alt="Logo"/>
+      </HashLink>
+      <div className={cx.navbar__hamburger} onClick={handleClick}>
+        <div className={isClicked ? cx.navbar__hamburger__line__active : cx.navbar__hamburger__line}></div>
       </div>
-    </BrowserRouter>
+      <nav>
+        <ul className={isClicked ? cx.navbar__menu__active : cx.navbar__menu}>
+          <li className={cx.navbar__menu__link}>
+            <HashLink smooth to="/#about">
+              About
+            </HashLink>
+          </li>
+          <li className={cx.navbar__menu__link}>
+            <HashLink smooth to="/#experience">
+              Experience
+            </HashLink>
+          </li>
+          <li className={cx.navbar__menu__link}>
+            <HashLink smooth to="/#projects">
+              Projects
+            </HashLink>
+          </li>
+          <li className={cx.navbar__menu__link}>
+            <HashLink smooth to="/#contact">
+              Contact
+            </HashLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
